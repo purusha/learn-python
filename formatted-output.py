32f698080bd3a6ed7cb9b9912d304c580a8f7fa6
Alan Toro <alan.toro@ext.contactlab.com>
2017-03-31 08:42:16 +0200

    output && structure

---
+++ formatted-output.py
@@ -0,0 +1,9 @@
+#importo la print di python3 (anche se uso python2)
+from __future__ import print_function
+
+p = 459
+q = 121.425
+
+print(q, p, p * q)
+print(q, p, p * q, sep=" ~ ")
+print("decimal is %4.2f" % q)
---
+++ structure.py
@@ -0,0 +1,21 @@
+list = [["a",["b",["c","x"]]]]
+print(list)
+
+tuple = ("tuples", "are", "immutable")
+print(tuple)
+
+str = "Python under Linux is great"
+print(str[::3])
+
+abc = ["a","b","c","d","e"]
+print("a" in abc)
+print("a" not in abc)
+
+food = {"ham" : "yes", "egg" : "yes", "spam" : "no" }
+print(food)
+
+for key in food:
+    print("%s => %s" % (key, food[key]))
+
+print(food.keys())
+print(food.values())
